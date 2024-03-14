7410f391105d0f672d0ff8758088a6bd7c7631de
Paweł K <grylion02@gmail.com>
2024-03-14 17:06:06 +0100

    Add tests for home page, test data, pytest.ini

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,10 @@
 import pytest
 from playwright.sync_api import sync_playwright
-@pytest.fixture(autouse=True, scope="session")
-def setup_browser():
+@pytest.fixture(autouse=True, scope="function") ## fixture will be run for each method
+def setup_browser_fixture():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
         page = browser.new_page()
         yield page
         browser.close()
+
