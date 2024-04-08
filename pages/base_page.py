3416e955ad5bdbf304b94637a083cb61a74885ad
Paweł K <grylion02@gmail.com>
2024-04-08 18:26:02 +0200

    changes in home page tests

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,13 +1,16 @@
 import pytest
 from playwright.sync_api import Page
 
+from data import urls
+from data.urls import Urls
+
 
 @pytest.mark.usefixtures("data_texts_for_elements_fixture")
 class BasePage:
     def __init__(self, page: Page):
         self.page = page
 
-    def navigate(self, url="https://automationexercise.com/"):
+    def navigate(self, url=Urls.BASE_URL):
         self.page.goto(url)
         self.page.wait_for_load_state('load')
 
