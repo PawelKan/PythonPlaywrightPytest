53a95e83eeaf466cf653f599f2cb2d0086c74440
Paweł K <grylion02@gmail.com>
2024-04-09 16:51:22 +0200

    add_page_manager_class

---
+++ pages/page_manager.py
@@ -0,0 +1,22 @@
+from playwright.sync_api import Page
+
+from pages.home_page import HomePage
+from pages.login_page import LoginPage
+from pages.register_account_success_page import RegisterAccountSuccessPage
+from pages.register_page import RegisterPage
+
+
+class PageManager:
+    def __init__(self, page: Page):
+        self.__home_page = HomePage(page)
+        self.__login_page = LoginPage(page)
+        self.__register_page = RegisterPage(page)
+        self.__register_account_success_page = RegisterAccountSuccessPage(page)
+
+    def on_home_page(self): return self.__home_page
+
+    def on_login_page(self): return self.__login_page
+
+    def on_register_page(self): return self.__register_page
+
+    def on_register_account_success_page(self): return self.__register_account_success_page
